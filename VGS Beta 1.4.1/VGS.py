05c3aa6b12785410ef37078d09283ed74b9620ea
Fernando Mateo Valencia Gómez <98895066+FernandoMVG@users.noreply.github.com>
2022-05-26 01:03:03 -0500

    Add files via upload

--- VGS Beta 1.4.1/VGS.py
+++ VGS Beta 1.4.1/VGS.py
@@ -3,7 +3,6 @@ import csv
 class Vigi_Senado():
     def __init__(self):
         self.SENADO = Lista()
-    
 
     def BuscarP(self, nombre):
         P = self.SENADO.head
@@ -13,15 +12,62 @@ class Vigi_Senado():
                 cont = False
             else:
                 P = P.next
-        print(f"Nombre: '{P.nombreP}' Numero de Senadores: {P.nSenadores} Fecha de Fundación: {P.fechaFundacion} Presidente: {P.presidente} Posición: {P.posicion} Eslogan: {P.eslogan}, Porcentaje: {P.porcentaje}")
-   
+        if(P == None):
+            print("No existe este partido")
+        else:
+            print(f"Nombre: {P.nombreP} \nNumero de Senadores: {P.nSenadores} \nFecha de Fundación: ", 
+            f"{P.fechaFundacion}  \nPresidente:  {P.presidente}  \nPosición:  {P.posicion}",
+            f" \nEslogan:  {P.eslogan}  \nPorcentaje:  {P.porcentaje}")
     
+    def BuscarC(self, nombre, partido):
+        P = self.SENADO.head
+        cont = True
+        i = 0
+        if (partido == None):
+            while((P.next != None)):
+                Q = P.child
+                while (Q != None and cont):
+                    if(nombre in Q.Nombre):
+                        print(f"{Q.Nombre}")
+                        Q = Q.child
+                        i = 1
+                    else:
+                        Q = Q.child
+                if(Q == None):
+                    P = P.next
+            if (i == 0):
+                print("No se encontraron senadores con este nombre")
+        else:
+            while((P.next != None) and (cont)):
+                if(P.nombreP == partido):
+                    cont = False
+                    Q = P.child
+                    while (Q != None):
+                        
+                        if(nombre in Q.Nombre):
+                            print(f"{Q.Nombre}")
+                            Q = Q.child
+                            i = 1
+                        else:
+                            Q = Q.child
+                else:
+                    P = P.next
+            if (i == 0):
+                print("No se encontraron senadores con este nombre dentro de este partido",
+                "pruebe buscando todos los senadores (Opción 2 luego Opción 1")
+        
+    def MostrarPartidos(self):
+        
+        ...
+    
+    def MostrarSenadores(self):
+        ...
+
     def Comparar():
         ...
     
     def CrearLista(self):
-        
-        with open("Partidos_Politicos.csv", "r") as archivoPar:
+        with open("Par_directorio.csv", "r") as archivoPar:
             lector = csv.reader(archivoPar, delimiter=";")
             #Omite el encabezado
             next(lector, None)
@@ -45,12 +91,12 @@ class Vigi_Senado():
                 NOMBRE = fila[1]
                 GENERO = fila[2]
                 AÑONACIMIENTO = fila[3]
-                DEPNACIMIENTO = fila[6]
-                CIUDADNACIMIENTO = fila[7] 
-                PARTIDOP = fila[8]
-                NVOTOS = fila[9]
-                CORREO = fila[12]
-                REDES = fila[13]
+                DEPNACIMIENTO = fila[5]
+                CIUDADNACIMIENTO = fila[6] 
+                PARTIDOP = fila[7]
+                NVOTOS = fila[8]
+                CORREO = fila[11]
+                REDES = fila[12]
                 #Se agrega el senador
                 self.SENADO.insertC(NOMBRE, GENERO, AÑONACIMIENTO, CIUDADNACIMIENTO,
                 DEPNACIMIENTO, PARTIDOP, PERIODO, NVOTOS, CORREO, REDES)
@@ -70,7 +116,7 @@ class Congresista():
         self.nVotos = nVotos
         self.correo = correo 
         self.redes = redes
-
+        
         self.next = self
         self.prev = self
         self.child = self
@@ -90,7 +136,6 @@ class Partido():
         self.next = self
         self.prev = self
         self.child = self
-
 
 class Lista:
     def __init__(self):
@@ -139,13 +184,14 @@ class Lista:
             P = self.head
             cont = True
             while ((P.next != None) and (cont)):
-                if(P.nombreP == nombre):
+                if(P.nombreP == partidoP):
+                    Q = P
                     if(P.child == None):
                         P.child = Congresista(nombre, genero, añoNacimiento,
                         ciudadNacimiento, depNacimiento, partidoP, periodo,
                         nVotos, correo, redes)
                         Q = Q.child
-                        Q.child = None
+                        Q.child = None                       
                     else:
                         Q = P.child
                         while (Q.child !=  None):
@@ -155,13 +201,54 @@ class Lista:
                         nVotos, correo, redes)
                         Q = Q.child
                         Q.child = None
-                    P.nSenadores = int(int(P.nSenadores) + 1)
-                    P.porcentaje = str((int(P.nSenadores) / 106) * 100) + "%"
+                    P.nSenadores += 1
+                    P.porcentaje = str((float(P.nSenadores) / 106) * 100) + "%"
                     cont = False
                 else:
                     P = P.next
 
 vgs = Vigi_Senado()
 vgs.CrearLista()
-print(vgs.SENADO.__repr__())
-vgs.BuscarP("Cambio")
+
+"""print(vgs.SENADO.__repr__())
+vgs.BuscarP("Colombia Humana")"""
+
+
+#Menu
+print("Bienvenido a Vigi-Senado")
+print("\n\n1: Partidos")
+print("2: Senadores")
+opc = int(input("Escoja una opción: "))
+if (opc == 1):
+    print("\nPartidos Politicos")
+    print("1: Buscar Partido")
+    print("2: Mostrar Partidos")
+    print("3: Volver")
+    opc = int(input("Ingrese su opción: "))
+    if (opc == 1):
+        nombreP = input("Ingrese Nombre del Partido: ")
+        vgs.BuscarP(nombreP)
+        print("\n1: Mostrar Senadores")
+        print("2: Buscar Senador")
+        opcp = int(input("Ingrese su opción: "))
+        if(opcp == 1):
+            vgs.MostrarSenadores()
+        elif(opcp == 2):
+            nombreS = input("Ingrese nombre del senador: ")
+            vgs.BuscarC(nombreS, nombreP)
+    elif(opc == 2):
+        vgs.MostrarPartidos()
+    elif(opc == 3):
+        ...
+elif(opc == 2):
+    print("\nSenadores")
+    print("1: Mostrar todos los Senadores")
+    print("2: Buscar Senador en especifico")
+    print("3: Volver")
+    opc = int(input("Ingrese su opción: "))
+    if (opc == 1):
+        ...
+    elif (opc == 2):
+        ...
+    elif (opc == 3):
+        ...
